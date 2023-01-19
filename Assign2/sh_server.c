36f368aa17b72499544cf8c5ce2d39cb97fd458e
Shivansh Shukla <shivanshshukla60@kgpian.iitkgp.ac.in>
2023-01-19 19:04:31 +0530

    Done

--- Assign2/sh_server.c
+++ Assign2/sh_server.c
@@ -10,23 +10,30 @@
 #include <dirent.h>
 
 const int MAXSIZE = 250, CHUNKSIZE = 50;                                                               
-char* buffer;                                                               // buffer for taking input from user
-
+                                                            
 int findMin(int a, int b)
 {
     return (a < b) ? a : b;
 }
 
-char* handle_pwd()
+char* handle_pwd(char* argument)
 {
-    if(getcwd(buffer, MAXSIZE) == NULL)                                     // In case of error
-    return "####";
-    return buffer;
+    char* res = (char*)malloc(sizeof(char) * MAXSIZE);
+    while(argument[0] == ' ' || argument[0] == '\t')
+    argument++;
+
+    if(argument[0] != '\0')                                                 // Invalid command
+    strcpy(res, "$$$$");
+    else if(getcwd(res, MAXSIZE) == NULL)                                   // In case of error
+    strcpy(res, "####");
+
+    return res;
 }
 
 void handle_dir(int newsockfd, char* argument)
 {
     char *temp_buf = (char*)malloc(sizeof(char) * MAXSIZE);
+
     if(argument[0] != ' ' && argument[0] != '\t' && argument[0] != '\0')    // Invalid command with dir as prefix
     {
         strcpy(temp_buf, "$$$$");
@@ -34,14 +41,14 @@ void handle_dir(int newsockfd, char* argument)
         return;
     }
 
+    while(argument[0] == ' ' || argument[0] == '\t')
+    argument++;
+
     if(argument[0] == '\0')                                                 // If there is no argument, then dir for current directory
     {
         argument[0] = '.';
         argument[1] = '\0';
     }
-
-    while(argument[0] != '\0' && (argument[0] == ' ' || argument[0] == '\t'))
-    argument++;
 
     struct dirent* dirEntry;                                                // Directory entry pointer
     DIR * dr = opendir(argument);
@@ -52,33 +59,36 @@ void handle_dir(int newsockfd, char* argument)
         return;
     }
     
-    int cnt, totalSent;
+    int cnt;
     while((dirEntry = readdir(dr)) != NULL)                                 // Iterating through all
     {
+        temp_buf = (char*)calloc(MAXSIZE, sizeof(char));
         strcpy(temp_buf, dirEntry->d_name);
+        cnt = strlen(temp_buf)+1; 
 
-        cnt = strlen(temp_buf)+1;                                           
         temp_buf[cnt-1] = '\n';
+
         for(int i = 0; i < 5; i++)                                          // Sending a chunk of 50 bytes in each iteration
         {
             if(i*CHUNKSIZE >= cnt)                                          // Checking if we have already sent the whole buffer in prev iteration
             break;  
-            totalSent = 0;
-            while(1)                                                        // Loop for sending expression to server
-            {
-                int temp = send(newsockfd, temp_buf+totalSent+CHUNKSIZE*i, findMin(CHUNKSIZE, cnt-i*CHUNKSIZE)-totalSent, 0);
-                totalSent += temp;
-                if(totalSent == findMin(CHUNKSIZE, cnt-i*CHUNKSIZE))       // Keep sending until total no. of char sent become equal to cntChars
-                break;
-            }
+            send(newsockfd, temp_buf, findMin(cnt-i*CHUNKSIZE, CHUNKSIZE), 0);
         }  
+        free(temp_buf);
     }
-    temp_buf[0] = '\0';                                                     // To denote end of information transfer in client side
-    send(newsockfd, temp_buf, 1, 0);
+    char str[1];                                                            // To denote end of result send to client side
+    str[0] = '\0';
+    send(newsockfd, str, 1, 0);
 }
 
 char* handle_cd(char* argument)
 {
+    if(argument[0] != ' ' && argument[0] != '\t' && argument[0] != '\0')    // Invalid command with cd as prefix
+    return "$$$$";
+
+    while(argument[0] == ' ' || argument[0] == '\t')
+    argument++;
+
     if(argument[0] == '\0')                                                 // If there is no argument, then cd to home directory
     {
         struct passwd *pword = getpwuid(getuid());
@@ -90,12 +100,6 @@ char* handle_cd(char* argument)
         return "\0";
     }
 
-    if(argument[0] != ' ' && argument[0] != '\t')                           // Invalid command with cd as prefix
-    return "$$$$";
-
-    while((argument[0] == ' ' || argument[0] == '\t') && argument[0] != '\0')
-    argument++;
-
     if(chdir(argument) < 0)                                                 // In case of error
     return "####";
 
@@ -106,7 +110,7 @@ int main()
 {
     int sockfd, newsockfd;                                                  // socket descriptors
     struct sockaddr_in servAddr, cliAddr;
-    buffer = (char*)malloc(MAXSIZE*sizeof(char));                     
+    char* buffer = (char*)malloc(MAXSIZE*sizeof(char));                     // buffer for taking input from user                  
 
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
@@ -126,9 +130,9 @@ int main()
 
     listen(sockfd, 5);                                                      // Atmost 5 clients can be queued while server is running
 
-    int cliLength, temp, idx, cnt; char validUser;
+    int cliLength, temp, idx, cnt, totalSent; char validUser;
     char* temp_buf = (char*)malloc(CHUNKSIZE * sizeof(char));
-    char* result;
+    char* result; 
     while(1)
     {
         if((newsockfd = accept(sockfd, (struct sockaddr* )&cliAddr, &cliLength)) < 0)
@@ -154,9 +158,7 @@ int main()
 
             while(fgets(temp_buf, MAXSIZE, fileptr) != NULL)                // Checking whether username is valid or not
             {
-                temp_buf[strcspn(temp_buf, " \t\n")] = '\0';
-                // printf("%s %d\n", temp_buf, (int)strlen(temp_buf));
-                // printf("%d %s %s\n", strcmp(temp_buf, buffer), temp_buf, buffer);
+                temp_buf[strcspn(temp_buf, " \t\n")] = '\0';                // Removing trailing space and tab
                 if(strcmp(temp_buf, buffer) == 0)
                 {
                     validUser = 't';
@@ -164,12 +166,15 @@ int main()
                 }
             }
             fclose(fileptr);
+
             if(validUser == 'f')
             {
+                printf("Invalid User\n");
                 strcpy(buffer, "NOT-FOUND");
                 temp = send(newsockfd, buffer, strlen(buffer)+1, 0);
                 exit(0);
             }
+            printf("Valid User\n");
             strcpy(buffer, "FOUND");
             temp = send(newsockfd, buffer, strlen(buffer)+1, 0);            
 
@@ -179,7 +184,6 @@ int main()
                 while(1)                                                        // For receiving command from client
                 {
                     cnt = recv(newsockfd, temp_buf, CHUNKSIZE, 0);
-                    printf("%d\n", cnt);
                     if(cnt <= 0)                                                // if client socket gets closed or some error in recv
                     {
                         printf("Error in receiving command from client...\n");
@@ -199,8 +203,8 @@ int main()
                     printf("Bye!\n");
                     break;
                 }
-                else if(strlen(buffer) == 3 && buffer[0] == 'p' && buffer[1] == 'w' && buffer[2] == 'd')
-                result = handle_pwd();
+                else if(strlen(buffer) >= 3 && buffer[0] == 'p' && buffer[1] == 'w' && buffer[2] == 'd')
+                result = handle_pwd(buffer+3);
                 else if(strlen(buffer) >= 3 && buffer[0] == 'd' && buffer[1] == 'i' && buffer[2] == 'r')
                 {
                     handle_dir(newsockfd, buffer+3);
@@ -211,7 +215,20 @@ int main()
                 else
                 result = "$$$$";
 
-                send(newsockfd, result, strlen(result)+1, 0);
+                cnt = strlen(result)+1;
+                for(int i = 0; i < 5; i++)                                      // Sending a chunk of 50 bytes in each iteration
+                {
+                    if(i*CHUNKSIZE >= cnt)                                      // Checking if we have already sent the whole buffer in prev iteration
+                    break;  
+                    totalSent = 0;
+                    while(1)                                                    // Loop for sending expression to server
+                    {
+                        temp = send(newsockfd, result+totalSent+CHUNKSIZE*i, findMin(CHUNKSIZE, cnt-i*CHUNKSIZE)-totalSent, 0);
+                        totalSent += temp;
+                        if(totalSent == findMin(CHUNKSIZE, cnt-i*CHUNKSIZE))                       // Keep sending until total no. of char sent become equal to cntChars
+                        break;
+                    }
+                }  
             }
             exit(0);
         }
